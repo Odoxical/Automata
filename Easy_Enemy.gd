6a85e9e924c25e273d62f994d16a3b6bd39537cc
Odoxical <owewou02@kingshigh.school.nz>
2024-08-12 08:45:09 +1200

    t
    t

--- Easy_Enemy.gd
+++ Easy_Enemy.gd
@@ -1,10 +1,14 @@
 extends CharacterBody2D
 
 @export var speed = 150
+var health = 100
 
 func _process(delta):
 	var progress = get_parent().get_progress() # Get the current progress
 	get_parent().set_progress(progress + (speed * delta)) # Update the progress
 
 	if get_parent().get_progress_ratio() == 1:
+		queue_free()
+
+	if health <= 0:
 		queue_free()
--- plasma_bullet.gd
+++ plasma_bullet.gd
@@ -1,2 +1,23 @@
 extends CharacterBody2D
 
+var target
+var speed = 1000
+var path_name = ""
+var bullet_damage
+@onready var EnemySpawnerNode = get_tree().get_first_node_in_group("EnemySpawner")
+func _physics_process(delta):
+	#var EnemySpawnerNode = get_tree().get_root().get_node("Path/PathFollow2D/Enemy Spawner")
+	for i in EnemySpawnerNode.get_child_count():
+		if EnemySpawnerNode.get_child(i).name == path_name:
+			target = EnemySpawnerNode.get_child(i).get_child(0).get_child(0).global_position
+
+	velocity = global_position.direction_to(target) *speed
+
+	look_at(target)
+
+	move_and_slide()
+
+	
+
+func _on_area_2d_body_entered(body):
+	queue_free()
