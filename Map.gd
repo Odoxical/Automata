7b5a39de51a3594ecbe8dbd7b6d86c51d7e9f196
Odoxical <owewou02@kingshigh.school.nz>
2024-08-16 14:59:42 +1200

    Title Semi up

--- Map.gd
+++ Map.gd
@@ -1,4 +1,3 @@
 extends Node2D
 var current_wave = 0
 var enemies_in_wave = 0
-
--- plasma_bullet.gd
+++ plasma_bullet.gd
@@ -5,12 +5,13 @@ var speed = 1000
 var path_name = ""
 var bullet_damage
 @onready var EnemySpawnerNode = get_tree().get_first_node_in_group("EnemySpawner")
+#get_first_node_in_group("EnemySpawner")
 func _physics_process(delta):
 	#var EnemySpawnerNode = get_tree().get_root().get_node("Path/PathFollow2D/Enemy Spawner")
 	for i in EnemySpawnerNode.get_child_count():
 		if EnemySpawnerNode.get_child(i).name == path_name:
 			target = EnemySpawnerNode.get_child(i).get_child(0).get_child(0).global_position
-
+	
 	velocity = global_position.direction_to(target) *speed
 
 	look_at(target)
