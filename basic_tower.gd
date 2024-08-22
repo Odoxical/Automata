87ceb6143418b6090c2f61157c855701f542e68c
Odoxical <owewou02@kingshigh.school.nz>
2024-08-22 13:48:14 +1200

    Bullets Finally Work:)

--- basic_tower.gd
+++ basic_tower.gd
@@ -1,38 +1,65 @@
 extends Node2D
 
-var bullet = preload("res://plasma_bullet.tscn")
+var BULLET = preload("res://plasma_bullet.tscn")
 var bullet_damage = 5
 var path_name
 var current_targets = [] #targets in range
 var current #curently attacking
+@onready var shoot_timer: Timer = $ShootTimer
 
 func _on_tower_body_exited(body):
 	pass # Replace with function body.
 
 func _on_tower_body_entered(body):
+	if body.is_in_group("Enemy"):
+		current_targets.append(body)
+		if not current:
+			current = current_targets.pop_back()
 	
-	if "Easy_Enemy" in body.name:
-		var tempArray = []
-		current_targets = get_node("Tower").get_overlapping_bodies()
+	if current and shoot_timer.is_stopped():
+		shoot_timer.start()
+
+func update_target():
+	if not current:
+		current = current_targets.pop_back()
 		
-		for i in current_targets:
-			if "Enemy" in i.name:
-				tempArray.append(i)
 		
-		var current_targets = null
-		
-		for i in tempArray:
-			if current_targets == null:
-				current_targets = i.get_node("../")
-			else:
-				if i.get_parent().getprogress() > current_targets.get_progress():
-					current_targets = i.get_node("../")
-		
-		current = current_targets
-		path_name = current_targets.get_parent().name
-		
-		var temp_bullet = bullet.instantiate()
-		temp_bullet.path_name = path_name
-		temp_bullet.bullet_damage = bullet_damage
-		get_node("bulletcontainer").add_child(temp_bullet)
-		temp_bullet.global_position = $Aim.global_position
+	#if "Easy_Enemy" in body.name:
+		#var tempArray = []
+		#current_targets = get_node("Tower").get_overlapping_bodies()
+		#
+		#for i in current_targets:
+			#if "Enemy" in i.name:
+				#tempArray.append(i)
+		#
+		#var current_targets = null
+		#
+		#for i in tempArray:
+			#if current_targets == null:
+				#current_targets = i.get_node("../")
+			#else:
+				#if i.get_parent().getprogress() > current_targets.get_progress():
+					#current_targets = i.get_node("../")
+		#
+		#current = current_targets
+		#path_name = current_targets.get_parent().name
+		#
+		#var temp_bullet = bullet.instantiate()
+		#temp_bullet.path_name = path_name
+		#temp_bullet.bullet_damage = bullet_damage
+		#get_node("bulletcontainer").add_child(temp_bullet)
+		#temp_bullet.global_position = $Aim.global_position
+		#temp_bullet.target = body
+
+
+func _on_shoot_timer_timeout() -> void:
+	var bullet = BULLET.instantiate()
+	update_target()
+	if current == null:
+		return
+		shoot_timer.stop()
+	add_sibling(bullet)
+	bullet.global_position = global_position
+	bullet.look_at(current.global_position)
+	if current:
+		shoot_timer.start()
--- plasma_bullet.gd
+++ plasma_bullet.gd
@@ -1,24 +1,25 @@
-extends CharacterBody2D
+extends Area2D
 
 var target
 var speed = 1000
 var path_name = ""
 var bullet_damage
-@onready var EnemySpawnerNode = get_tree().get_first_node_in_group("EnemySpawner")
+#@onready var EnemySpawnerNode = get_tree().get_first_node_in_group("EnemySpawner")
 #get_first_node_in_group("EnemySpawner")
 func _physics_process(delta):
-	#var EnemySpawnerNode = get_tree().get_root().get_node("Path/PathFollow2D/Enemy Spawner")
-	for i in EnemySpawnerNode.get_child_count():
-		if EnemySpawnerNode.get_child(i).name == path_name:
-			target = EnemySpawnerNode.get_child(i).get_child(0).get_child(0).global_position
+	##var EnemySpawnerNode = get_tree().get_root().get_node("Path/PathFollow2D/Enemy Spawner")
+	#for i in EnemySpawnerNode.get_child_count():
+		#if EnemySpawnerNode.get_child(i).name == path_name:
+			#target = EnemySpawnerNode.get_child(i).get_child(0).get_child(0).global_position
 	
-	velocity = global_position.direction_to(target) *speed
+	translate(transform.x * speed * delta)
+	
 
-	look_at(target)
-
-	move_and_slide()
 
 	
 
 func _on_area_2d_body_entered(body):
-	queue_free()
+	if body.is_in_group("Enemy"):
+		body.health -= bullet_damage
+		queue_free()
+		
