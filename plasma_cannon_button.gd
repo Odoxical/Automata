117fb2152f03d20f39a856e21acd700183850217
Odoxical <owewou02@kingshigh.school.nz>
2024-09-02 13:25:28 +1200

    Semi Functional UI

--- plasma_cannon_button.gd
+++ plasma_cannon_button.gd
@@ -5,4 +5,14 @@ var currtile
 
 func _on_gui_input(event: InputEvent):
 	var temptower = tower.instantiate
-	print(event)
+	if event is InputEventMouseButton and event.button_mask == 1:
+		add_child(temptower)
+		temptower.global_position = event.global_position
+		temptower.process_mode = Node.PROCESS_MODE_DISABLED
+		#left Click Pressed
+	elif event is InputEventMouseMotion and event.button_mask == 1:
+		pass
+		#Drag
+	elif event is InputEventMouseButton and event.button_mask == 0:
+		pass
+		#Left Click Released
